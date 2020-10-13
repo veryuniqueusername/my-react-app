2a83d8aed9b7266cf4a596a6e42eb479dfcaba2c
TigerWolf <henning.magnus.nilsson@gmail.com>
2020-10-13 08:23:13 +0200

    base

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -12,19 +12,48 @@ export default function Schedule() {
 
 function ScheduleViewer() {
 	const SCHEDULE = require('./Schedule.json');
+	var items: any = [];
 
 	for (let day = 0; day < SCHEDULE.length; day++) {
 		for (let data = 0; data < SCHEDULE[day].length; data++) {
-			scheduleCalc(
-				SCHEDULE[day][data].subject,
-				SCHEDULE[day][data].start,
-				SCHEDULE[day][data].end
+			items.push(
+				scheduleCalc(
+					SCHEDULE[day][data].subject,
+					SCHEDULE[day][data].teacher,
+					SCHEDULE[day][data].room,
+					SCHEDULE[day][data].start,
+					SCHEDULE[day][data].end
+				)
 			);
 		}
 	}
-	return <div className="Schedule"></div>;
+	return <div className="Schedule"> {items}</div>;
 }
 
-function scheduleCalc(name: string, start: Array<number>, end: Array<number>) {
-	return <div></div>;
+function scheduleCalc(
+	name: string,
+	teacher: string,
+	room: string,
+	start: Array<number>,
+	end: Array<number>
+) {
+	var starting = start[0] * 60 + start[1];
+
+	if (name === 'LANG') {
+		return (
+			<div className={name}>
+				<span>SP MSG 128</span>
+				<span>FR JDN 116</span>
+				<span>TY AAD 118</span>
+				<span>SV/EN EWM 212</span>
+				<span>SV/EN FJT 231</span>
+			</div>
+		);
+	}
+
+	return (
+		<div className={name}>
+			{name} {teacher} {room}
+		</div>
+	);
 }
