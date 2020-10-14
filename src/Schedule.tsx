348b9c3f6087ec2f522e97294b349a1e3a09f721
TigerWolf <henning.magnus.nilsson@gmail.com>
2020-10-14 21:17:06 +0200

    1st version

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -40,45 +40,33 @@ function ScheduleViewer(props) {
 	}
 
 	for (let data = 0; data < SCHEDULE[dayIndex].length; data++) {
-		items.push(
-			scheduleCalc(
-				SCHEDULE[dayIndex][data].subject,
-				SCHEDULE[dayIndex][data].teacher,
-				SCHEDULE[dayIndex][data].room,
-				SCHEDULE[dayIndex][data].start,
-				SCHEDULE[dayIndex][data].end
-			)
-		);
+		items.push(scheduleCalc(SCHEDULE[dayIndex][data]));
 	}
 	return <div className="Day">{items}</div>;
 }
 
-function scheduleCalc(
-	name: string,
-	teacher: string,
-	room: string,
-	start: Array<number>,
-	end: Array<number>
-) {
+function scheduleCalc(obj) {
+	// INIT
+	var start = obj.start;
+	var end = obj.end;
+	var name = obj.subject;
+	var teacher = obj.teacher;
+	var room = obj.room;
+
+	// BUNCH OF STYLING AND POSITIONING
 	var starting = ((start[0] - 6) * 60 + start[1]) * (5 / 27) * 0.85;
 	var ending = ((end[0] - 6) * 60 + end[1]) * (5 / 27) * 0.85;
-	var length = (ending - starting);
+	var length = ending - starting;
 	var classes = name + ' Subject';
-	console.log(starting);
 	var styles = {
 		height: length + '%',
 		top: starting + '%',
+		width: 'calc((20% - 8px))',
 	};
 
+	// IF LANG
 	if (name === 'LANG') {
 		return (
-			// <div className={classes}>
-			// 	<span>SP MSG 128</span>
-			// 	<span>FR JDN 116</span>
-			// 	<span>TY AAD 118</span>
-			// 	<span>SV/EN EWM 212</span>
-			// 	<span>SV/EN FJT 231</span>
-			// </div>
 			<div className={classes} style={styles}>
 				<span>Språkval</span>
 				<span></span>
@@ -86,12 +74,49 @@ function scheduleCalc(
 			</div>
 		);
 	}
-
-	return (
-		<div className={classes} style={styles}>
-			<span>{name}</span>
-			<span>{teacher}</span>
-			<span>{room}</span>
-		</div>
-	);
+	// IF ONE SUBJECT
+	if (!obj.subject2) {
+		return (
+			<div className={classes} style={styles}>
+				<span>{name}</span>
+				<span>{teacher}</span>
+				<span>{room}</span>
+			</div>
+		);
+	}
+	// IF TWO SUBJECTS
+	else {
+		// INIT
+		var start2 = obj.start2;
+		var end2 = obj.end2;
+		var name2 = obj.subject2;
+		var teacher2 = obj.teacher2;
+		var room2 = obj.room2;
+		// BUNCH OF STYLING AND POSITIONING
+		var starting2 = ((start2[0] - 6) * 60 + start2[1]) * (5 / 27) * 0.85;
+		var ending2 = ((end2[0] - 6) * 60 + end2[1]) * (5 / 27) * 0.85;
+		var length2 = ending2 - starting2;
+		var classes2 = name2 + ' Subject';
+		styles.width = 'calc((20% - 8px) / 2)';
+		var styles2 = {
+			height: length2 + '%',
+			top: starting2 + '%',
+			width: 'calc((20% - 8px) / 2)',
+			transform: 'translateX(100%)'
+		};
+		return (
+			<div>
+				<div className={classes} style={styles}>
+					<span>{name}</span>
+					<span>{teacher}</span>
+					<span>{room}</span>
+				</div>
+				<div className={classes2} style={styles2}>
+					<span>{name2}</span>
+					<span>{teacher2}</span>
+					<span>{room2}</span>
+				</div>
+			</div>
+		);
+	}
 }
